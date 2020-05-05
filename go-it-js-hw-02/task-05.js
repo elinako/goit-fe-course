6108ad0053bf063253bcb54dea5b149d05b2c18f
Elina <elisonfog@gmail.com>
2020-05-05 22:08:31 +0300

    added tasks to hw-02

--- go-it-js-hw-02/task-05.js
+++ go-it-js-hw-02/task-05.js
@@ -1,5 +1,23 @@
-let a = "Curabitur ligula sapien, tincidunt non.";
-let Arr = a.split("");
-console.log(Arr);
-let Brr = Arr.join("");
-console.log(Brr);
+const SPAM = "spam";
+const SALE = "sale";
+
+const checkForSpam = function (message) {
+  const newMessage = message.toLowerCase();
+  const messageArr = newMessage.split(" ");
+  let result = false;
+  for (const element of messageArr) {
+    if (element.includes(SPAM) || element.includes(SALE)) {
+      result = true;
+    }
+  }
+
+  return result;
+};
+
+console.log(checkForSpam("Latest technology news"));
+
+console.log(checkForSpam("JavaScript weekly newsletter"));
+
+console.log(checkForSpam("Get  best sale offers now!"));
+
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
--- go-it-js-hw-02/task-06.js
+++ go-it-js-hw-02/task-06.js
@@ -1,11 +1,20 @@
 let input;
 let total = 0;
+let numbers = [];
+let arr;
 let message;
-const array = [];
 
 while (input !== null) {
   const promptMessage = "Введите, пожалуйста, любое число:";
   input = prompt(promptMessage);
-  total = Number(input) + total;
+  numbers.push(Number(input));
+  arr = numbers;
+  console.log(arr);
+}
+
+for (let i = 0; i < arr.length; i += 1) {
+  total = total + arr[i];
   message = `Общая сумма чисел равна ${total}`;
 }
+
+alert(message);
