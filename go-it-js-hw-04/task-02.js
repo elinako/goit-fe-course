8b4422e348e84df22f35c295233724b74d10070d
Elina <elisonfog@gmail.com>
2020-05-18 23:53:52 +0300

    added js-hw-04

---
+++ go-it-js-hw-04/task-02.js
@@ -0,0 +1,33 @@
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+const boundAdd = inventory.add.bind(inventory);
+const boundRemove = inventory.remove.bind(inventory);
+
+invokeInventoryAction("Medkit", boundAdd);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", boundRemove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
